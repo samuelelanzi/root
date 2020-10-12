0fb004b36c544d455256517358346313d6e294ca
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-12 14:14:52 +0200

    main

--- code/particle/main.cpp
+++ code/particle/main.cpp
@@ -1,6 +1,10 @@
 #include "ParticleType.hpp"
+#include "ResonanceType.hpp"
 
 int main() { 
     ParticleType particle {"electron", 9.109e-31, -1};
     particle.print();
+    
+    ResonanceType resonance {particle.getName(), particle.getMass(), particle.getCharge(), 23.78};
+    resonance.print();
 }
