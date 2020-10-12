3d3ea6401f7f2ac4b1d804f38087777125c80448
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-12 14:15:40 +0200

    ResonanceType

--- code/particle/ResonanceType.hpp
+++ code/particle/ResonanceType.hpp
@@ -1,5 +1,17 @@
 #ifndef RESONANCETYPE_HPP
 #define RESONANCETYPE_HPP
 
+#include "ParticleType.hpp"
+
+class ResonanceType : public ParticleType {
+protected:
+    double width_;
+public:
+    ResonanceType(std::string name, double mass, int charge, double width);
+
+    double getWidth();
+
+    void print() override;
+};
 
 #endif
