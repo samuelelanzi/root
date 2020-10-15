642b339ca0e2584ef2e61417210424f718fb7b16
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-15 18:24:07 +0200

    Particle

--- code/particle/Particle.hpp
+++ code/particle/Particle.hpp
@@ -5,16 +5,6 @@
 
 class Particle {
 private:
-    static ParticleType* ParticleType_[];
-    static int MaxNumParticleType_;
-    static int NParticleType_;
-    int IParticle_;
-    double Px_;
-    double Py_;
-    double Pz_;
 public:
-    Particle(std::string name);
-    ~Particle();
-    int getIParticle();
 };
 #endif
