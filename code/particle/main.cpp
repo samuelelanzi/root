d29ce116bc7563b411ace5af48a5dac0a15e73af
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-15 18:25:33 +0200

    main

--- code/particle/main.cpp
+++ code/particle/main.cpp
@@ -1,10 +1,1 @@
-#include "ParticleType.hpp"
-#include "ResonanceType.hpp"
-
-int main() { 
-    ParticleType particle {"electron", 9.109e-31, -1};
-    particle.print();
-
-    ResonanceType resonance {particle, 23.78};
-    resonance.print();
-}
+int main() { }
