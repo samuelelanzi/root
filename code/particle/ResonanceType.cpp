0302898adafd423b47de0c6a7e52fec6ff1508cc
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-15 18:25:05 +0200

    ResonanceType

--- code/particle/ResonanceType.cpp
+++ code/particle/ResonanceType.cpp
@@ -1,9 +1,1 @@
 #include "ResonanceType.hpp"
-
-ResonanceType::ResonanceType(ParticleType const& particle, double width) : ParticleType{particle}, width_{width} {}
-
-double ResonanceType::getWidth() { return width_; }
-
-void ResonanceType::print() {
-    std::cout << "width: " << width_ << '\n';
-}
