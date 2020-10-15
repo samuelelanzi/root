5c7e214a61ca980f678654e1c5e4dbef9b5d4746
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-15 18:24:41 +0200

    ParticleType

--- code/particle/ParticleType.hpp
+++ code/particle/ParticleType.hpp
@@ -1,21 +1,10 @@
 #ifndef PARTICLETYPE_HPP
 #define PARTICLETYPE_HPP
-
 #include <iostream>
 
 class ParticleType {
 protected:
-    std::string name_;
-    double mass_;
-    int charge_;
-
 public:
-    ParticleType(std::string name, double mass, int charge);
-    std::string getName();
-    double getMass();
-    int getCharge();
-
-    virtual void print();
 };
 
 #endif
