0dacfc46cd5ba26db25a11f2d6bc140196ba0203
samuelemerola <69164971+samuelemerola@users.noreply.github.com>
2020-10-21 11:25:14 +0200

    Update main.cpp

--- code/particle/main.cpp
+++ code/particle/main.cpp
@@ -1,9 +1,16 @@
 #include "ParticleType.hpp"
 #include "ResonanceType.hpp"
+#include <vector>
 
 int main() {
     ParticleType particle {"electron", 9.1e-31, -1};
-    particle.Print();
-    ResonanceType resonance {particle, 0.};
-    resonance.Print();
+  //  particle.Print();
+    ResonanceType resonance {particle, 3.2};
+    //resonance.Print();
+    
+    std::vector <ParticleType> ParticleV {particle, resonance};
+    
+    for (auto i: ParticleV){
+        i.Print();
+    }
 }
