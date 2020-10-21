587fad4de07a0e1c57209b9167aec6f78d062452
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-21 10:38:18 +0200

    main

--- code/particle/ParticleType.cpp
+++ code/particle/ParticleType.cpp
@@ -1,1 +1,21 @@
 #include "ParticleType.hpp"
+
+ParticleType::ParticleType(std::string name, double mass, int charge) : fName{name}, fMass{mass}, fCharge{charge} {}
+
+std::string ParticleType::getName() {
+    return fName;
+}
+
+double ParticleType::getMass() {
+    return fMass;
+}
+
+int ParticleType::getCharge() {
+    return fCharge;
+}
+
+void ParticleType::Print() {
+    std::cout << "Name: " << fName << '\n';
+    std::cout << "Mass: " << fMass << '\n';
+    std::cout << "Charge: " << fCharge << '\n';
+}
