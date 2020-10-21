bfe516f14cd32b8a703f5a7df2d85cda498c0029
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-21 11:23:51 +0200

    main

--- code/particle/main.cpp
+++ code/particle/main.cpp
@@ -1,9 +1,15 @@
 #include "ParticleType.hpp"
 #include "ResonanceType.hpp"
+#include <vector>
 
 int main() {
-    ParticleType particle {"electron", 9.1e-31, -1};
-    particle.Print();
-    ResonanceType resonance {particle, 0.};
-    resonance.Print();
+    ParticleType* particle = new ParticleType {"electron", 9.1e-31, -1};
+    //particle.Print();
+    ResonanceType* resonance = new ResonanceType {*particle, 0.};
+    //resonance.Print();
+    std::vector<ParticleType*> particle_v {particle, resonance};
+
+    for(auto i : particle_v) {
+        i -> Print();
+    }
 }
