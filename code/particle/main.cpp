f2b8d0005631b1f15e2be0bdad364b9474f94e52
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-21 10:38:07 +0200

    main

--- code/particle/main.cpp
+++ code/particle/main.cpp
@@ -1,1 +1,9 @@
-int main() { }
+#include "ParticleType.hpp"
+#include "ResonanceType.hpp"
+
+int main() {
+    ParticleType particle {"electron", 9.1e-31, -1};
+    particle.Print();
+    ResonanceType resonance {particle, 0.};
+    resonance.Print();
+}
