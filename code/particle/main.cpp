e510160fd8e3ce3c1e265c48e7bcbc11e28f9dc7
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-22 12:09:18 +0200

    upload

--- code/particle/main.cpp
+++ code/particle/main.cpp
@@ -1,14 +1,26 @@
 #include "ParticleType.hpp"
 #include "ResonanceType.hpp"
+#include "Particle.hpp"
 #include <vector>
 
 int main() {
-    ParticleType* particle = new ParticleType {"electron", 9.1e-31, -1};
-    ResonanceType* resonance = new ResonanceType {*particle, 0.};
+    ParticleType* electron = new ParticleType {"electron", 9.109e-31, -1};
+    ResonanceType* e_resonance = new ResonanceType {*electron, 0.};
+    ParticleType* proton = new ParticleType {"proton", 1.673e-27, 1};
+    ResonanceType* p_resonance = new ResonanceType {*proton, 0.};
 
-    std::vector<ParticleType*> particle_v {particle, resonance};
+    std::vector<ParticleType*> particle_v {};
+
+    particle_v.push_back(electron);
+    particle_v.push_back(e_resonance);
+    particle_v.push_back(proton);
+    particle_v.push_back(p_resonance);
 
     for(auto i : particle_v) {
         i -> Print();
     }
+
+    P l_momentum {2.4, 4.8, 0.};
+
+    Particle par {particle_v, "electron", l_momentum};
 }
