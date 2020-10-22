ea0770993ea1d99518135709d68923fec8362366
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-22 19:06:10 +0200

    upload

--- code/particle/Particle.cpp
+++ code/particle/Particle.cpp
@@ -1,5 +1,28 @@
 #include "Particle.hpp"
 
 Particle::Particle(std::vector<ParticleType*> particle_v, std::string name, P p) : fParticleType{particle_v}, fName{name}, fP{p} {
-    FindParticle(particle_v);
+    ParticleType* result = FindParticle(particle_v);
+    if (result != nullptr) {
+        std::cout << "Find " << result -> getName() << ' ' << "at the " << fNParticleType << "-th " << "position" << '\n';
+    } else {
+        std::cout << "Not Find" << '\n';
+    }
+}
+
+P Particle::getP() { return fP; }
+
+void Particle::setP(double const& px, double const& py, double const& pz) {
+    fP.fPx = px;
+    fP.fPy = py;
+    fP.fPz = pz;
+}
+
+double Particle::Energy() {
+    auto p = FindParticle(fParticleType);
+    double m = p -> getMass();
+    double lm_x = fP.fPx;
+    double lm_y = fP.fPy;
+    double lm_z = fP.fPz;
+    double E = std::sqrt( (m * m) + (lm_x * lm_x) + (lm_y * lm_y) + (lm_z * lm_z) );
+    return E;
 }
