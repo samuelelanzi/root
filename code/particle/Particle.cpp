cb3e4181654fc6bb804491ebc6f9297490050f5c
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-22 12:09:26 +0200

    upload

--- code/particle/Particle.cpp
+++ code/particle/Particle.cpp
@@ -1,5 +1,5 @@
 #include "Particle.hpp"
 
-Particle::Particle(std::vector<ParticleType*> particle_v, P p) : fParticleType{particle_v}, fP{p} {
-    
+Particle::Particle(std::vector<ParticleType*> particle_v, std::string name, P p) : fParticleType{particle_v}, fName{name}, fP{p} {
+    FindParticle(particle_v);
 }
