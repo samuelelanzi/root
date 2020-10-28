ea83e002724c1667063a9103664460045c7a7abc
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-28 09:59:02 +0100

    upload

--- code/particle/invMass.cpp
+++ code/particle/invMass.cpp
@@ -1,5 +1,19 @@
 #include "invMass.hpp"
 
-double invMass(Particle const& p1, Particle const& p2) {
-	
+double invMass(Particle p1, Particle p2) {
+	double E1 = p1.Energy();
+    double E2 = p2.Energy();
+
+    P P1 = p1.getP();
+    P P2 = p2.getP();
+
+    double sumE = std::pow(E1 + E2, 2);
+
+    double sumPx = P1.fPx + P2.fPx;
+    double sumPy = P1.fPy + P2.fPy;
+    double sumPz = P1.fPz + P2.fPz;
+
+    double modP = std::sqrt(sumPx * sumPx + sumPy * sumPy + sumPz * sumPz);
+
+    return std::sqrt(sumE * sumE - modP * modP);
 }
