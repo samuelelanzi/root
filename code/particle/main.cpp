67a8d2e5fab07fad02e5ea52df07905ddf8a3f19
samuelelanzi <samuelelanzi00@icloud.com>
2020-10-30 22:53:30 +0100

    upload

--- code/particle/main.cpp
+++ code/particle/main.cpp
@@ -7,86 +7,40 @@
 #include <random>
 
 int main() {
-  ParticleType *electron = new ParticleType{"electron", 9.109e-31, -1};
-  // ResonanceType* e_resonance = new ResonanceType {*electron, 0.};
-  ParticleType *proton = new ParticleType{"proton", 1.673e-27, 1};
-  // ResonanceType* p_resonance = new ResonanceType {*proton, 0.};
-  ParticleType *neutron = new ParticleType{"neutron", 1.675e-27, 0};
-  // ResonanceType* n_resonance = new ResonanceType {*neutron, 0.};
+  std::vector<ParticleType*> particle_v{};
 
-  std::vector<ParticleType *> particle_v{};
-
-  particle_v.push_back(electron);
-  // particle_v.push_back(e_resonance);
-  particle_v.push_back(proton);
-  // particle_v.push_back(p_resonance);
-  particle_v.push_back(neutron);
-  // particle_v.push_back(n_resonance);
-
-  for (auto i : particle_v) {
-    i->Print();
-  }
-
-  P e_linearMomentum;
-  P p_linearMomentum;
-  P n_linearMomentum;
-
-  Particle e{particle_v, "electron", e_linearMomentum, 0};
-  Particle p{particle_v, "proton", p_linearMomentum, 1};
-  Particle n{particle_v, "neutron", n_linearMomentum, 2};
-
-  e.setP(1., 1., 1.);
-  p.setP(2., 2., 2.);
-  n.setP(1., 2., 1.);
-
-  std::cout << '\n';
-
-  e.printParticle();
-  p.printParticle();
-  n.printParticle();
-
-  std::cout << "Electron Energy: " << e.Energy() << '\n';
-  std::cout << "Proton Energy: " << p.Energy() << '\n';
-  std::cout << "Neutron Energy: " << n.Energy() << '\n';
-
-  // std::cout << e.getIParticle() << '\n';
-  // std::cout << p.getIParticle() << '\n';
-  // std::cout << n.getIParticle() << '\n';
-
-  double invEP = invMass(e, p);
-  std::cout << "\nMass invariant electron/proton: " << invEP << '\n';
+  // Particle e{particle_v, "electron", e_linearMomentum, 0};
   
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> distrib(1,100);
   int seed = distrib(gen);
   
-
-
   gRandom->SetSeed(seed);
 
   for(int i = 0; i != 10e5; ++i) {
     for(int j = 0; j != 10e2; ++j) {
       int prob_type = distrib(gen);
       int charge = rndmCharge(prob_type);
-      std::cout << charge;
-      if (prob_type <= 80) {
-      } else if (prob_type > 80 && prob_type <= 90) {
-
-      } else if (prob_type > 90 && prob_type <= 99) {
-
-      } else {
-
-      }
 
       auto phi = gRandom->Uniform(0., 2 * M_PI);
-      // std::cout << phi << '\n';
       auto theta = gRandom->Uniform(0., M_PI);
-      // std::cout << theta << '\n';
       auto p_ = gRandom->Exp(1);
-      // std::cout << p_ << '\n';
 
+      if (prob_type <= 80) {
+        ParticleType* pion = new ParticleType {"pion", 0.13957, charge};
+        particle_v.push_back(pion);
+      } else if (prob_type > 80 && prob_type <= 90) {
+        ParticleType* kaon = new ParticleType {"kaon", 0.49367, charge};
+        particle_v.push_back(kaon);
+      } else if (prob_type > 90 && prob_type <= 99) {
+        ParticleType* proton = new ParticleType {"proton", 0.93827, charge};
+        particle_v.push_back(proton);
+      } else {
+        ParticleType* K_s = new ParticleType {"K*", 0.89166, 0};
+        ResonanceType* K_resonance = new ResonanceType {*K_s, 0.050};
+        particle_v.push_back(K_s);
+      }
     }
   }
-
 }
