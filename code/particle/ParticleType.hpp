495d771233e3a01cb98e495693b0706eeca37eab
samuelelanzi <samuelelanzi00@icloud.com>
2020-11-08 19:18:44 +0100

    upload

--- code/particle/ParticleType.hpp
+++ code/particle/ParticleType.hpp
@@ -4,15 +4,16 @@
 
 class ParticleType {
 protected:
-  std::string const fName;
-  double const fMass;
-  int const fCharge;
+  std::string fName;
+  double fMass;
+  int fCharge;
 
 public:
   ParticleType(std::string name, double mass, int charge);
   std::string getName();
   double getMass();
   int getCharge();
+  void setCharge(int &charge);
   virtual double getWidth();
   virtual void Print();
 };
